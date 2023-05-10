4914b023cb37cca62634c3ea0e566da7e274fcb5
Daniel <danielseniv@outlook.com>
2023-05-09 22:21:01 -0400

    Use sum, average, and size methods to fetch calculated data from database

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,23 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
+=begin
+### Exercise 5: Calculations
+
+1. Output the total revenue for the entire company (all stores), using Active Record's `.sum` calculation method.
+2. On the next line, also output the average annual revenue for all stores.
+3. Output the number of stores that are generating $1M or more in annual sales. **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
+=end
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+
+puts @total_revenue
+
+@average_revenue = Store.average(:annual_revenue)
+
+puts @average_revenue
+
+@expense_stores = Store.where("annual_revenue >= ?", 1000000).size()
+
+puts @expense_stores
